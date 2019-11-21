87de3ddca1a898ba1d1882ef49010b07759ee488
MUHAMMAD FURQAN <31932590+FURQAN2050@users.noreply.github.com>
2019-11-22 00:07:24 +0500

    basic Structure.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StudentMainComponent } from './sms/Students/student-main/student-main.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'students',component:StudentMainComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './sms/components/navbar/navbar.component';
+import { SidebarComponent } from './sms/components/sidebar/sidebar.component';
+import { StudentMainComponent } from './sms/Students/student-main/student-main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    StudentMainComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
